e074dda89b980bbbaca7389c28c11f92d15c4ae0
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:26:41 +0000

    Update MainFrame.java

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class MainFrame {
+package software.ulpgc.kata3;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+	private HistogramDisplay histogramDisplay;
+
+	public MainFrame() throws HeadlessException {
+		this.setTitle("Histogram viewer");
+		this.setSize(800,600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.add(createHistogramDisplay());
+	}
+
+	private Component createHistogramDisplay() {
+		JFreeChartHistogramDisplay display = new JFreeChartHistogramDisplay();
+		this.histogramDisplay = display;
+		return display;
+	}
+
+	public HistogramDisplay histogramDisplay() {
+		return histogramDisplay;
+	}
 }

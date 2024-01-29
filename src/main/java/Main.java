9163ff80b006a848992375b01f51c72c57193e00
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:26:05 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+package software.ulpgc.kata3;
+
+public class Main {
+	public static void main(String[] args) {
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.histogramDisplay().show(histogram());
+		mainFrame.setVisible(true);
+	}
+
+	private static Histogram histogram() {
+		return new Histogram() {
+			@Override
+			public int bins() {
+				return 5;
+			}
+
+			@Override
+			public double[] values() {
+				return new double[] { 1,2,1,3,4,3,2,1,5,4,3,7,8,5,4,7,1};
+			}
+		};
+	}
 }
+
